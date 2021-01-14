8c0b1cc371a19e58a698d6026cfe7c92e228a86d
shilkawi <71741556+shilkawi@users.noreply.github.com>
2021-01-14 10:12:31 -0500

    Add files via upload

---
+++ platformer/game.py
@@ -0,0 +1,307 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+#define game variables
+tile_size = 50
+game_over = 0
+main_menu = True
+
+
+
+#load images
+moon_img = pygame.image.load(r'images\moon.png')
+bg_img = pygame.image.load(r'images\bikini.png')
+restart_img = pygame.image.load(r'images\restart.png')
+start_img = pygame.image.load(r'images\start.png')
+exit_img = pygame.image.load(r'images\exit.png')
+win_img = pygame.image.load(r'images\win.png')
+win_img = pygame.transform.scale(win_img, (300, 300))
+
+
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clicked = False
+
+    def draw(self):
+        action = False
+
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                action = True
+                self.clicked = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+                
+
+        screen.blit(self.image, self.rect)
+
+        return action
+
+
+class Player():
+    def __init__(self, x, y):
+        self.reset(x, y)
+
+    def update(self, game_over):
+        dy = 0
+        dx = 0
+
+        if game_over == 0:
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+                dx -= 3
+            if key[pygame.K_RIGHT]:
+                dx += 3
+            if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
+                self.vel_y = -15
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
+
+            #collision
+            self.in_air = True
+            for tile in world.tile_list:
+                
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+            
+            
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    if self.vel_y<0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    elif self.vel_y>0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+                        self.in_air = False
+
+
+            if pygame.sprite.spritecollide(self, ball_group, False):
+                game_over = -1
+            
+
+            if pygame.sprite.spritecollide(self, fire_group, False):
+                game_over = -1
+
+            if pygame.sprite.spritecollide(self, exit_group, False):
+                game_over = 1
+            
+            
+
+
+
+
+                    
+            
+            self.rect.x += dx
+            self.rect.y += dy
+
+            if self.rect.bottom > screen_height:
+                self.rect.bottom = screen_height
+
+        
+        
+        screen.blit(self.image, self.rect)
+        return game_over
+
+    def reset(self, x, y):
+        img = pygame.image.load(r'images\player.png')
+        self.image = pygame.transform.scale(img, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.in_air = True
+
+
+class World():
+    def __init__(self, data):
+
+        self.tile_list = []
+
+        #load image
+        border_img = pygame.image.load(r'images\border.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(border_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 3:
+                    ball = Enemy(col_count * tile_size, row_count * tile_size+10)
+                    ball_group.add(ball)
+                if tile == 6:
+                    fire = Fire(col_count * tile_size, row_count * tile_size+25)
+                    fire_group.add(fire)
+                    
+                if tile == 8:
+                    exit = Exit(col_count * tile_size, row_count * tile_size-20)
+                    exit_group.add(exit)
+                    
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+            
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(r'images\enemy.png')
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+    
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if self.move_counter > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
+
+class Fire(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load(r'images\fire.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    
+class Exit(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load(r'images\door.png')
+        self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+
+
+world_data = [ 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 7, 1, 1, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1], 
+[1, 7, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+[1, 0, 1, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 1, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 1, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 1, 1, 1, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+
+
+player = Player(100, screen_height - 50)
+
+ball_group = pygame.sprite.Group()
+fire_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
+
+world = World(world_data)
+
+restart_button = Button(screen_width//2 - 50, screen_height//2 + 100, restart_img)
+start_button = Button(screen_width//2 - 350, screen_height//2, start_img)
+exit_button = Button(screen_width//2 + 150, screen_height//2, exit_img)
+
+
+run = True
+while run:
+
+    clock.tick(fps)
+    
+    screen.blit(bg_img,(0, 0))
+    screen.blit(moon_img,(0, 0))
+
+    if main_menu == True:
+        if exit_button.draw():
+            run = False
+
+        if start_button.draw():
+            main_menu = False
+    
+    else:
+        world.draw()
+
+        if game_over == 0:
+            ball_group.update()
+
+        ball_group.draw(screen)
+        fire_group.draw(screen)
+        exit_group.draw(screen)
+        
+        game_over = player.update(game_over)
+
+        if game_over == -1:
+            if restart_button.draw():
+                player.reset(100, screen_height - 50)
+                game_over = 0
+
+        if game_over == 1:
+            screen.blit(win_img,(300, 100))
+            if restart_button.draw():
+                player.reset(100, screen_height - 50)
+                game_over = 0
+        
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
+
+
